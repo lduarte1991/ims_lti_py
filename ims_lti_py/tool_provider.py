16af6e9ca33b987038f344d42e34407a0ff2d9f6
lduarte1991 <lduarte1991@gmail.com>
2019-04-16 13:47:59 -0400

    dot notation and urllib changes

--- ims_lti_py/tool_provider.py
+++ ims_lti_py/tool_provider.py
@@ -1,15 +1,14 @@
-from launch_params import LaunchParamsMixin
-from request_validator import (
+from .launch_params import LaunchParamsMixin
+from .request_validator import (
     RequestValidatorMixin,
     FlaskRequestValidatorMixin,
     DjangoRequestValidatorMixin,
     WebObRequestValidatorMixin
 )
-from outcome_request import OutcomeRequest
+from .outcome_request import OutcomeRequest
 from collections import defaultdict
 import re
-from urllib import urlencode
-from urlparse import urlsplit, urlunsplit
+from urllib.parse import urlencode, urlsplit, urlunsplit
 
 try:
     from urlparse import parse_qsl
